b016f83b3a2cf79823e21a2edd430e6f2e2a39f7
M.Mahdi Saeidi <M.Mahdisaeidi99@gmail.com>
2024-06-29 14:09:25 +0330

    feat(deploy): complete deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,9 +5,52 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 const hre = require("hardhat");
+const { ethers } = require('hardhat');
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), 'ether')
+}
 
 async function main() {
+  // Setup accounts
+  const [buyer, seller, inspector, lender] = await ethers.getSigners();
 
+  // Deploy Real Estate
+  const RealEstate = await ethers.getContractFactory('RealEstate');
+  const realEstate = await RealEstate.deploy();
+  await realEstate.deployed();
+
+  console.log(`Deployed Real Estate Contract at: ${realEstate.address}`);
+  console.log(`Minting 3 properties...\n`)
+
+  for (let index = 0; index < 3; index++) {
+    const transaction = await realEstate.connect(seller).mint(`https://ipfs.io/ipfs/bafybeiew5ksfyiutnn4ocrmpmtiorbuadsrt5fjkue2twahir2qipwxhsq/${index + 1}.json`)
+    await transaction.wait();
+  }
+
+  // Deploy Escrow
+  const Escrow = await ethers.getContractFactory('Escrow')
+  const escrow = await Escrow.deploy(realEstate.address, seller.address, inspector.address, lender.address);
+  await escrow.deployed();
+
+  console.log(`Deployed Escrow Contract at: ${escrow.address}`);
+
+  for (let index = 0; index < 3; index++) {
+    // Approve properties...
+    let transaction = await realEstate.connect(seller).approve(escrow.address, index + 1);
+    await transaction.wait()
+  }
+
+  // Listing properties...
+  let transaction = await escrow.connect(seller).list(1, buyer.address, tokens(20), tokens(10))
+  await transaction.wait()
+
+  transaction = await escrow.connect(seller).list(2, buyer.address, tokens(15), tokens(5))
+  await transaction.wait()
+
+  transaction = await escrow.connect(seller).list(3, buyer.address, tokens(10), tokens(5))
+  await transaction.wait()
+
+  console.log('Finished.')
 }
 
 // We recommend this pattern to be able to use async/await everywhere
